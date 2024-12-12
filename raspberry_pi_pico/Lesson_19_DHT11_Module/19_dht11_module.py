d94b712756600e6ef15a0175c814b4211dbee7ca
wulu <wulu@wulu.zone>
2024-12-12 10:05:34 +0800

    fix dht11 example

--- raspberry_pi_pico/Lesson_19_DHT11_Module/19_dht11_module.py
+++ raspberry_pi_pico/Lesson_19_DHT11_Module/19_dht11_module.py
@@ -1,29 +1,13 @@
+import dht
+import machine
 import time
-import board
-import adafruit_dht
 
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D17)
+# Initialize DHT11 sensor on GPIO 16
+d = dht.DHT11(machine.Pin(16))
 
+# Continuously read and print temperature and humidity
 while True:
-    try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
-
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
-
-    time.sleep(2.0)
+    d.measure()
+    print("Temperature:" ,d.temperature())  # Print temperature
+    print("Humidity:" ,d.humidity())  # Print humidity
+    time.sleep_ms(1000)  # Read every second
